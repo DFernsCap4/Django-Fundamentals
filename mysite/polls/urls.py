2da96c83339af71cf0582768f3d4f621c52592ae
DFernsCap4 <142505558+DFernsCap4@users.noreply.github.com>
2024-06-26 11:58:07 +0200

    Day three

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = "polls"
 urlpatterns= [
     path("", views.IndexView.as_view(), name="index"),
+    path("registration/", views.registration, name="registration"),
+    path("register/", views.register, name="register"),
+    path("login/", views.loginForm, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("authenticateUser", views.authenticateUser, name="authenticateUser"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,13 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -12,10 +16,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Question.objects.order_by("-pub_date")[:5]
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
 
-class DetailView(generic.DetailView):
+class DetailView(LoginRequiredMixin ,generic.DetailView):
     model = Question
     template_name= "polls/detail.html"
+    
+    login_url='/polls/login/'
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -34,3 +45,36 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after succesfully dealing with PostData
         # This prevents data from being posted twice if a user hits the Back button
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+def registration(request):
+    context={}
+    return render(request, "polls/register.html",context)
+
+def register(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    email = request.POST["email"]
+    user = User.objects.create_user(username, email, password)
+    user.save()
+
+    login(request, user)
+
+    return HttpResponseRedirect(reverse("polls:index"))
+
+def loginForm(request):
+    context= {}
+    return render(request, "polls/login.html", context)
+
+def authenticateUser(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("polls:index"))
+    else:
+        return render(request,"polls/login.html", {"error_message": "Username or password incorrect"})
+
+def logoutUser(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("polls:index"))
